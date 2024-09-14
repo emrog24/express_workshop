8e82464cdad43f0952cb70b118fdd88a4166a248
PanaBlackxer <emanuel356justo@gmail.com>
2024-09-14 12:27:45 -0600

    request added by POST and bodyParser

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const { pokemon } = require('./pokedex.json');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 /*
 INTERCAMBIOS DE PETICIONES:
@@ -16,32 +19,29 @@ DELETE - Borrar un recurso
 app.get("/", (req, res, next)=>{
     return res.status(200).send("Bienvenido a la Pokedex");
 });
-app.get("/pokemon/all", (req, res, next)=>{// te muestra todos los pokemones
+
+app.post("/pokemon", (req, res, next)=>{
+    return res.status(200).send(req.body);
+});
+
+app.get("/pokemon", (req, res, next)=>{//sirve para que te muestre todos los pokemones
     return res.status(200).send(pokemon);
 });
 
 app.get('/pokemon/:id([0-9]{1,3})', (req, res, next)=>{//sirve para que te muestre el pokemon por id
-    const id = req.params.id - 1;
-    if(id>=0 && id<=150){
-        res.status(200);
-        res.send(pokemon[req.params.id - 1]);
-    }
-    else{
-        res.status(404);
-        res.send("Pokemon no encontrado");
-    }
+    const id = req.params.id -1;
+    (id >= 0 && id <= 150) ? res.status(200).send(pokemon[req.params.id-1]) :
+    res.status(404).send("Pokemon no encontrado");
 });
+
 app.get('/pokemon/:name([A-Za-z]+)', (req, res, next)=>{//sirve para que te muestre el pokemon por nombre
     const name = req.params.name;
-    for(let i = 0; i<pokemon.length; i++){
-        if(name == pokemon[i].name){
-            res.status(200);
-            res.send(pokemon[i]);
-        }
-    }
-    res.status(404);
-    res.send("Pokemon no encontrado");
+    const pk = pokemon.filter((p) =>{
+        return (p.name.toUpperCase()==name.toUpperCase()) ? p : null;
+    });
+    (pk.length>0) ? res.status(200).send(pk) : res.status(404).send("Pokemon no encontrado...")
 });
+
 app.listen(process.env.PORT || 3000, ()=>{
     console.log("Server is Running...")
 })
