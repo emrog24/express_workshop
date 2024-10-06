77ad15e3bb8fafc1539b6fe0b0e7205039493045
PanaBlackxer <emanuel356justo@gmail.com>
2024-10-05 22:09:55 -0600

    Added resouce USER

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const morgan = require('morgan');
 const express = require('express');
 const app = express();
 const pokemon = require('./routers/pokemon');
+const user = require('./routers/user');
 const e = require('express');
+
+
 
 app.use(morgan('dev'));
 //app.use(express.json());
@@ -26,6 +29,7 @@ app.get("/", (req, res, next)=>{
 });
 
 app.use("/pokemon", pokemon);
+app.use("/user", user);
 
 app.use((req, res, next)=>{
     return res.status(404).json({ code: 404, message: "URL no encontrada"});
