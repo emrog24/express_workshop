fe0bdd4871af90011f357dcacf8b3e6a0befcb8a
emrog24 <emrog243@gmail.com>
2024-10-19 16:17:15 -0600

    Interfas de usuario 1

--- index.js
+++ index.js
@@ -10,9 +10,10 @@ const e = require('express');
 const auth = require('./middleware/auth');
 const notFound = require('./middleware/notFound');
 const index = require('./middleware/index');
+const cors = require('./middleware/cors');
 
 
-
+app.use(cors);
 app.use(morgan('dev'));
 //app.use(express.json());
 //app.use(express.urlencoded({extended: true}));
